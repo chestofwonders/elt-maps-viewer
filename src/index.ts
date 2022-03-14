314cdc3ce48c4adc5d8622469e3a085493ad9c07
Miguel Arroyo <miguel.arroyo@plainconcepts.com>
2022-03-14 10:43:33 +0000

    Azure deployment

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+import 'leaflet/dist/leaflet.css';
+import './styles/main.css';
+import L from 'leaflet';
+
+const mymap = new L.Map('map').setView([40.433531, -3.6953617], 17);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);

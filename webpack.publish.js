4b9362c7906735626aa737d8d8f3085cb7ea8f13
Miguel Arroyo <miguel.arroyo@plainconcepts.com>
2022-03-16 17:11:56 +0100

    Lib deployment

--- webpack.publish.js
+++ webpack.publish.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const { merge } = require("webpack-merge");
 
 const baseConfig = require("./webpack.config.js");
@@ -5,6 +6,9 @@ const baseConfig = require("./webpack.config.js");
 module.exports = merge(baseConfig, {
   mode: "production",
   output: {
+    filename: "elt-maps-viewer.js",
+    path: path.resolve(__dirname, "lib"),
+    clean: true,
     library: 'MapsViewer',
     libraryTarget: 'umd',
     globalObject: 'this'
